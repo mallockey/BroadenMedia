5ad3afe6fd305f86c5a1a1ca12600a46587909ad
Joshua Melo <joshuamelo@gmail.com>
2021-01-13 13:46:57 -0500

    fixed about page

--- pages/About.js
+++ pages/About.js
@@ -10,7 +10,7 @@ const About = () => {
         <div id="homeMain">
           <p id="aboutPara">
             This site was built with the current state of US politics in mind. We believe that the only way to truth is by examining
-            all perspectives and then coming to your own conclusion. Our determinations of each sources political affilation is determiend by
+            all perspectives and then coming to your own conclusion. Each sources political affilation is determined by
             <a id="aboutLink" href="https://allsides.com"> Allsides</a>
           </p>
         </div>
